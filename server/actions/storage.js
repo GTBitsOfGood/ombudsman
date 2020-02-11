1476d0e99570938bb94a33c8a1131563f1ecf366
Kris Satya <krissatya@Kriss-MacBook-Pro.local>
2020-02-11 18:58:11 -0500

    db updates

--- server/actions/storage.js
+++ server/actions/storage.js
@@ -1,15 +1,18 @@
 import firebase from "firebase";
 import "firebase/storage";
+import "firebase/firestore"
 
 if (!firebase.apps.length) {
   const firebaseConfig = {
     apiKey: "AIzaSyDu2fblA5PCmdknt6reohIMeOlqgf-B1No",
     // authDomain: '<your-auth-domain>',
+    projectId: 'ombudsman-a8077',
     storageBucket: "gs://ombudsman-a8077.appspot.com"
   };
   firebase.initializeApp(firebaseConfig);
 }
 const storage = firebase.storage();
+const db = firebase.firestore();
 
 export const getPDF = async () => {
   const storageRef = await storage.ref().list({ maxResults: 100 });
