b064aa8426fc14c20c9034a1e911df01fc05248f
jbaldwin2014 <jbaldwin2014@fau.edu>
2020-02-17 02:31:28 -0500

    Added dropdown for categories, still need to center content.

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,6 +4,8 @@ import Link from 'next/link';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import Dropdown from 'react-bootstrap/Dropdown';
 import { getPDF } from '../client/actions/api';
 
 const SSRPage = ({ message, errorMessage }) => (
@@ -15,6 +17,13 @@ const SSRPage = ({ message, errorMessage }) => (
           <FormControl type="text" placeholder="Search" className="mr-sm-2" />
           <Button variant="outline-success">Search</Button>
         </Form>
+        <br></br>
+        <DropdownButton id="dropdown-basic-button" title="Select Category">
+          <Dropdown.Item href="#/category-1">category 1</Dropdown.Item>
+          <Dropdown.Item href="#/category-2">category 2</Dropdown.Item>
+          <Dropdown.Item href="#/category-3">category 3</Dropdown.Item>
+        </DropdownButton>
+        <br></br>
         Files:
         {message.map((item) => (
           <li key={item.fileName}>
