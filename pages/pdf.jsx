4baa01ad6edfef403b364b306ac476868639d661
Michael Chen <michael.ld.chen@gmail.com>
2020-03-08 20:15:50 -0400

    Try Mozilla's online pdf.js

--- pages/pdf.jsx
+++ pages/pdf.jsx
@@ -7,13 +7,16 @@ import Link from 'next/link';
 import { getPDF, getCategories, updateClicks } from '../client/actions/api';
 
 // We can also use an iframe with src="http://docs.google.com/gview?url=yourPdfUrl&embedded=true", but this always says "No Preview Available" and doesn't work for files larger than 25 MB
+// https://github.com/mozilla/pdf.js/tree/master/examples/webpack
+// https://github.com/mozilla/pdf.js/blob/master/examples/webpack/main.js
+// https://github.com/mikecousins/react-pdf-js does not support selecting text
+// https://stackoverflow.com/a/32518877/5139284
+// https://github.com/mozilla/pdf.js/wiki/Frequently-Asked-Questions#can-i-load-a-pdf-from-another-server-cross-domain-request
 const pdfPage = ({ category, fileName, url, errorMessage }) => {
   return (
     <div height="100%" style={{ overflow: 'auto', height: '100%' }}>
       {errorMessage == null ? (
-        <object data={url} type="application/pdf" height="99%" width="100%">
-          <embed src={url} type="application/pdf" />
-        </object>
+        <iframe src={/* `https://mozilla.github.io/pdf.js/web/viewer.html?file=${encodeURI(url)}` */ 'https://mozilla.github.io/pdf.js/web/viewer.html?file=https://www.who.int/docs/default-source/coronaviruse/who-china-joint-mission-on-covid-19-final-report.pdf'} title={fileName} height="100%" width="100%" frameBorder="0" />
       ) : (
         <h4>
           SSR Error: {errorMessage}
