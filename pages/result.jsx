08ce2fdd026f5557a7e3663d1c5b97eca1b4e995
jbaldwin2014 <jbaldwin2014@fau.edu>
2020-03-09 12:15:52 -0400

    Added placeholder link for open PDF on search page and made template for result page.

--- pages/result.jsx
+++ pages/result.jsx
@@ -8,8 +8,8 @@ import Form from 'react-bootstrap/Form';
 import Link from 'next/link';
 import { getPDF, getCategories, updateClicks } from '../client/actions/api';
 
-const searchPage = ({
-  pdfProps, name, pdfs, errorMessage, clickUpdate, categories,
+const resultPage = ({
+  pdfProps, name, errorMessage, clickUpdate, categories,
 }) => (
   <>
     {errorMessage == null
@@ -18,52 +18,30 @@ const searchPage = ({
           <Row>
             <Col>
               <a href="/">Home</a>
-              <a> / Search Results</a>
+              <a href="/search?pdfs=0&pdfs=0&pdfs=0&pdfs=0&pdfs=0&pdfs=0"> / Search Results</a>
+			  <a> / Document Title</a>
             </Col>
           </Row>
 		  <br /><br />
 		  <Row>
-          <Col>
-		  <Dropdown>
-			  <Dropdown.Toggle>
-				Filter By
-			  </Dropdown.Toggle>
-			  <Dropdown.Menu>
-				{
-				  categories.map((item, index) => (
-					<div>
-					  <Form.Check
-						className="dropdown-item"
-						onClick={() => {
-						  const currCheck = checked;
-						  currCheck[index] = (checked[index]) ? 0 : 1;
-						  setCheck([...currCheck]);
-						}}
-						value={item}
-						label={item}
-						filtertype="normalfilter"
-					  />
-					</div>
-				  ))
-				}
-			  </Dropdown.Menu>
-			</Dropdown>
-			</Col>
-            <Col md={{ span: 3 }}>
+            <Col>
 			<h2>
-			<Link href="/result">File Name</Link>
+			File Name
 			</h2>
 			<h3>Effective Date: 3/12/13</h3>
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam convallis est et ornare maximus. Morbi mattis, tellus maximus elementum mollis, magna sapien dictum ante, blandit aliquet neque sapien sit amet nulla. Vestibulum faucibus lorem non nulla lacinia, eu iaculis turpis sollicitudin. Nunc eget elit laoreet, malesuada risus quis, mollis leo. Nam sit amet sollicitudin elit. Integer est neque, scelerisque vel sem at, fringilla placerat ex. Nam cursus tortor sed diam tincidunt interdum.</p>
+			<h2>
+			<br />
+			Table of Contents
+			</h2>
             </Col>
-			<Col md={{ span: 1, offset: 3 }}>
+			<Col md={{ span: 1, offset: 2}}>
 			<div className="card card-block">insert pdf <br />preview here<br /><br /><br /></div><br />
 			<Link href="https://firebasestorage.googleapis.com/v0/b/ombudsman-a8077.appspot.com/o/LTCO%20Program%2FOlder%20Americans%20Act%20(Federal%20Law).pdf?alt=media&token=99424932-4bab-4ef2-b1ad-1abfa9dd785a" passHref>
 				<button type="button" className="btn btn-primary">OPEN PDF</button>
 			</Link>
 			</Col>
 			</Row>
-			<hr />
           {/* Keep for demo purposes
           Files:
           {Object.keys(pdfProps).map((category) => (
@@ -94,29 +72,26 @@ const searchPage = ({
   </>
 );
 
-searchPage.getInitialProps = async ({ query }) => {
+resultPage.getInitialProps = async () => {
   const pdfJ = await getPDF();
   const categoriesJ = await getCategories();
   const catArray = Object.keys(categoriesJ);
   const pdfMap = [];
-  query.pdfs.map((item, index) => {
-    if (item === '1') pdfMap.push(pdfJ.pdfMap[catArray[index]]);
-  });
-  return { pdfProps: pdfMap, pdfs: query.pdfs, categories: catArray, };
+  return { pdfProps: pdfMap, categories: catArray, };
 };
 
-searchPage.propTypes = {
+resultPage.propTypes = {
   pdfProps: PropTypes.arrayOf(Object),
   name: PropTypes.string,
   pdfs: PropTypes.arrayOf(Object),
   clickUpdate: PropTypes.func,
   errorMessage: PropTypes.string,
 };
-searchPage.defaultProps = {
+resultPage.defaultProps = {
   pdfProps: {},
   name: null,
   pdfs: [],
   clickUpdate: (data) => updateClicks(data.category, data.fileName),
   errorMessage: null,
 };
-export default searchPage;
+export default resultPage;
