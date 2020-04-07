3ae4dec400edcaa8100ae90088f293ede70b9a7e
Kris Satya <krissatya09@gmail.com>
2020-04-06 17:34:13 -0700

    fixed help page

--- pages/help.jsx
+++ pages/help.jsx
@@ -1,19 +1,15 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import PropTypes from 'prop-types';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Dropdown from 'react-bootstrap/Dropdown';
-import Form from 'react-bootstrap/Form';
+import Loading from '../client/components/Loading/Loading';
+import { PdfContext } from './context/pdf-context';
 
-import Link from 'next/link';
-import { getPDF, getCategories, updateClicks } from '../client/actions/api';
-
-const helpPage = ({
-  pdfProps, name, errorMessage, clickUpdate, categories,
-}) => (
-  <>
-    {errorMessage == null
-      ? (
+const helpPage = () => {
+  const [loading] = useContext(PdfContext);
+  return (<>
+    {loading ? (<Loading/>) : (
+      <>
         <div>
           <Row>
             <Col md={{ span: 5, offset: 3 }}>
@@ -34,35 +30,8 @@ const helpPage = ({
             </Col>
           </Row>
         </div>
-      ) : (
-        <h4>
-          SSR Error:
-          {errorMessage}
-        </h4>
+        </>
       )}
-  </>
-);
+  </>)};
 
-helpPage.getInitialProps = async () => {
-  const pdfJ = await getPDF();
-  const categoriesJ = await getCategories();
-  const catArray = Object.keys(categoriesJ);
-  const pdfMap = [];
-  return { pdfProps: pdfMap, categories: catArray, };
-};
-
-helpPage.propTypes = {
-  pdfProps: PropTypes.arrayOf(Object),
-  name: PropTypes.string,
-  pdfs: PropTypes.arrayOf(Object),
-  clickUpdate: PropTypes.func,
-  errorMessage: PropTypes.string,
-};
-helpPage.defaultProps = {
-  pdfProps: {},
-  name: null,
-  pdfs: [],
-  clickUpdate: (data) => updateClicks(data.category, data.fileName),
-  errorMessage: null,
-};
 export default helpPage;
