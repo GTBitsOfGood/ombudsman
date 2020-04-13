534694233ee03bbcaeb7408dad3d4fd3a225b10e
Michael Chen <michael.ld.chen@gmail.com>
2020-04-13 14:14:31 -0400

    Mobile responsive home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -10,11 +10,13 @@ import Link from 'next/link';
 import Loading from '../client/components/Loading/Loading';
 import { updateClicks } from '../client/actions/api';
 import { PdfContext } from './context/pdf-context';
+import useWindowDimensions from '../client/hooks/useWindowDimensions';
 
 const HomePage = ({ clickUpdate }) => {
   const [loading, pdfs, categories, sortedPdfs] = useContext(PdfContext);
   const [checked, setCheck] = useState([]);
   const [query, setQuery] = useState('');
+  const { height, width } = useWindowDimensions();
 
   useEffect(() => {
     if (!loading) setCheck(new Array(categories.length).fill(1));
@@ -34,8 +36,8 @@ const HomePage = ({ clickUpdate }) => {
                       <form onSubmit={suppressEvent} className="input-group" role="search">
                         <Form.Control type="text" placeholder="Search" onChange={(e) => (setQuery(e.target.value))} />
                         <Dropdown>
-                          <Dropdown.Toggle>
-                            Select a Category
+                          <Dropdown.Toggle className="btn-light" style={{ borderColor: '#adb3b8' }}>
+                            Categories
                           </Dropdown.Toggle>
                           <Dropdown.Menu>
                             <div onClick={() => {
@@ -84,46 +86,61 @@ const HomePage = ({ clickUpdate }) => {
                 </div>
                 <div className="freq-viewed" align="center">
                   <h4 align="center" className="my-4">Frequently Viewed Documents</h4>
-                  {
-                    [0, 1].map(row => {
-                      return <Row className="mb-3" key={row}>
-                        {
-                          [row * 3 + 0, row * 3 + 1,  row * 3 + 2].map(i => {
-                            return <Col key={i}>
-                              <div className="card card-block">
-                                {sortedPdfs[i].fileName}
-                                <p>
-                                  <Link href={{ pathname: '/render', query: { url: sortedPdfs[i].url } }}>
-                                    <a
-                                      onClick={() => clickUpdate(
-                                        { fileName: sortedPdfs[i].fileName, category: sortedPdfs[i].category })}
-                                    >
-                                      <br />
-                                      <button type="button" className="btn btn-primary">View</button>
-                                    </a>
-                                  </Link>
-                                </p>
-                              </div>
-                            </Col>;
-                          })
-                        }
-                      </Row>;
-                    })
+                  {width > 600 ?
+                    [0, 1].map(row =>
+                      <Row className="mb-3" key={row}>
+                        {[row * 3 + 0, row * 3 + 1,  row * 3 + 2].map(i =>
+                          <Col key={i}>
+                            <div className="card card-block">
+                              {sortedPdfs[i].fileName}
+                              <p>
+                                <Link href={{ pathname: '/render', query: { url: sortedPdfs[i].url } }}>
+                                  <a
+                                    onClick={() => clickUpdate(
+                                      { fileName: sortedPdfs[i].fileName, category: sortedPdfs[i].category })}
+                                  >
+                                    <br />
+                                    <button type="button" className="btn btn-primary">View</button>
+                                  </a>
+                                </Link>
+                              </p>
+                            </div>
+                          </Col>
+                        )}
+                      </Row>
+                    ) :
+                    [0, 1, 2, 3, 4, 5].map(i => 
+                      <div className="card card-block">
+                        {sortedPdfs[i].fileName}
+                        <p>
+                          <Link href={{ pathname: '/render', query: { url: sortedPdfs[i].url } }}>
+                            <a
+                              onClick={() => clickUpdate(
+                                { fileName: sortedPdfs[i].fileName, category: sortedPdfs[i].category })}
+                            >
+                              <button type="button" className="btn btn-primary">View</button>
+                            </a>
+                          </Link>
+                        </p>
+                      </div>
+                    )
                   }
                   <hr />
                 </div>
                 <div className="find-category">
                   <h4 align="center" className="my-4">Find by Category</h4>
-                  {categories.map((category, i) => {
-                    return (i * 2 < categories.length) ?
-                      <Row key={category}>
-                        <CategoryDropdown categoryName={categories[i * 2]} categoryPdfs={pdfs[categories[i * 2]]} align="right" />
-                        {(i * 2 + 1 < categories.length) ?
-                          <CategoryDropdown categoryName={categories[i * 2 + 1]} categoryPdfs={pdfs[categories[i * 2 + 1]]} align="left" />
-                        : null}
-                      </Row>
-                    : null;
-                  })}
+                  {categories.map((category, i) =>
+                    width > 900 ?
+                      ((i * 2 < categories.length) ?
+                        <Row key={category}>
+                          <CategoryDropdown categoryName={categories[i * 2]} categoryPdfs={pdfs[categories[i * 2]]} align="right" />
+                          {(i * 2 + 1 < categories.length) ?
+                            <CategoryDropdown categoryName={categories[i * 2 + 1]} categoryPdfs={pdfs[categories[i * 2 + 1]]} align="left" />
+                          : null}
+                        </Row>
+                      : null)
+                    : <CategoryDropdown categoryName={categories[i]} categoryPdfs={pdfs[categories[i]]} />
+                  )}
                 </div>
               </div>
             </>
