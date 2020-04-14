d4ba1e5508b9896005124ec0399f001877ef2541
Kris Satya <krissatya09@gmail.com>
2020-04-13 18:32:29 -0700

    responsive footer

--- client/components/Footer/Footer.jsx
+++ client/components/Footer/Footer.jsx
@@ -7,23 +7,21 @@ import PropTypes from 'prop-types';
 import Link from 'next/link';
 
 const Footer = ({ path }) => (
-  <Navbar sticky="bottom" bg="light">
-    <Navbar.Text>
-      <Nav activeKey={path}>
-        <Navbar.Brand>Ombudsman Toolbox</Navbar.Brand>
-        <Nav.Item>
-          <Nav.Link href={urls.pages.index}>Home</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="/help">Help</Nav.Link>
-        </Nav.Item>
-      </Nav>
-    </Navbar.Text>
-    <Navbar.Toggle />
-    <Navbar.Collapse className="justify-content-end">
-      <Navbar.Text>
-        <Link href="/login"><Button variant="outline-dark">ADD DOCUMENTS</Button></Link>
-      </Navbar.Text>
+  <Navbar sticky="bottom" bg="light" expand="md">
+    <Navbar.Brand>Ombudsman</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+    <Nav activeKey={path}>
+      <Nav.Item>
+        <Nav.Link href={urls.pages.index}>Home</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/help">Help</Nav.Link>
+      </Nav.Item>
+    </Nav>
+    <Nav className="ml-auto">
+      <Link href="/login"><Button variant="outline-dark">ADD DOCUMENTS</Button></Link>
+    </Nav>
     </Navbar.Collapse>
   </Navbar>
 );
